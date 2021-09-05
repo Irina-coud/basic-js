d3b0321b9fe21c5937188533b997c30da61c6487
Irina-coud <irinatobolich313@gmail.com>
2021-09-05 22:40:22 +0300

    task done 1/2

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,57 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (arr.length === 0) {
+    return arr;
+  }
+  if (!(arr instanceof Array)) {
+    throw new Error("Not implemented");
+  }
+  const discardNext = '--discard-next';
+  const discardPrev = '--discard-prev';
+  const doubleNext = '--double-next';
+  const doublePrev = '--double-prev';
+
+  if (!arr.includes(discardNext) &&
+    !arr.includes(discardPrev) &&
+    !arr.includes(doubleNext) &&
+    !arr.includes(doublePrev)) {
+    return arr;
+  }
+
+
+  let result = [];
+
+
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+
+    switch (element) {
+      case discardNext:
+        break;
+      case discardPrev:
+        if (result.length && arr[i - 2] !== discardNext) {
+          result.pop();
+        }
+        break;
+      case doubleNext:
+        if (i !== arr.length - 1) {
+          result.push(arr[i + 1]);
+        }
+        break;
+      case doublePrev:
+        if (i !== 0 && arr[i - 2] !== discardNext) {
+          result.push(arr[i - 1]);
+        }
+        break;
+      default:
+        if (arr[i - 1] !== discardNext) {
+          result.push(element);
+        }
+        break;
+    }
+  }
+
+  return result;
 }
